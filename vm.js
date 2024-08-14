342b1c9543d10d7befabea3af92212c90f75bf6a
Mathias Erligmann <merligmann@gmail.com>
2024-08-14 03:18:04 +0200

    cleaning

--- vm.js
+++ vm.js
@@ -1,7 +1,6 @@
 const crypto = require("crypto");
 const StorageTree = require("./tree/storage-tree");
 const { parse } = require("path");
-const res = require("express/lib/response");
 
 function hash(data) {
   return crypto.createHash("sha256").update(data).digest("hex");
